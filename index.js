55a34f5b69d923462da63e72636bcb0c7737d26e
Guilherme Meiring <101908636+GuiMeiring@users.noreply.github.com>
2024-05-15 10:18:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const products = [
     {
         id: 1,
         name: "Oculos de Realidade Virtual Oculus Quest 2, 256GB, Branco",
-        price: 6599.92,
+        price: 4129.92,
         mark:"Oculos",
-        portion: 7764.61,
+        portion: 4894.61,
         description: {
             color: "Branco",
             particulars:"Hardware de próximo nível - Faça com que cada movimento conte com um processador extremamente rápido e nossa tela de mais alta resolução Jogos All-In-One - Com compatibilidade com versões anteriores, você pode explorar novos títulos e antigos favoritos na extensa biblioteca de conteúdo da Quest Entretenimento imersivo - obtenha o melhor lugar da casa para shows ao vivo, filmes inovadores, eventos exclusivos e muito mais Configuração fácil - basta abrir a caixa, configurar o aplicativo do smartphone e pular para a realidade virtual. Nenhum PC ou console necessário. Requer acesso sem fio à Internet e o aplicativo Oculus (download gratuito) para configurar o dispositivo Tela Premium - capture todos os detalhes com uma tela impressionante que apresenta 50% mais pixels do que a Quest original Controle final - controladores Oculus Touch redesenhados transportam seus movimentos diretamente para a RV com controles intuitivos Compatível com PC VR - Entre em incríveis títulos Oculus Rift conectando um cabo Oculus Link a um PC de jogos compatível. Oculus Link Cable vendido separadamente Som cinematográfico 3D - Ouça em todas as direções com alto-falantes integrados que oferecem áudio posicional 3D cinematográfico",
@@ -24,9 +24,9 @@ const products = [
     {
         id: 2,
         name: "Oculos de Realidade Virtual Oculus GO, 512GB, Cinza",
-        price: 11646.97,
+        price: 4646.97,
         mark:"Oculus",
-        portion: 14348.28,
+        portion: 5348.28,
         description: {
             color: "Cinza",
             particulars:"HARDWARE - Não requer PC - Configure o aplicativo Oculus para celular e explore, assista e jogue em realidade virtual imediatamente. Não é necessário ter fios nem um computador.RASTREAMENTO - Três graus de liberdade - Com 3DoF, o headset rastreia os movimentos da cabeça e os converte em realidade virtual para visualização em 360°. É recomendado jogar sentado. Não é necessário usar sensores externos. CONTROLES - Controle único - Esse controle leve reconhece o movimento das mãos e as ações de apontar e clicar, aperfeiçoando a navegação na realidade virtual.ALÇA PARA CABEÇA - Alça macia - Fácil de ajustar e projetada para ser imperceptível. A alça macia oferece conforto e leveza para todas as pessoas.SOM - Áudio espacial - Spatial audio cinematográfico integrado diretamente ao headset, permitindo que você ouça tudo ao seu redor. A porta de áudio de 3,5 mm permite jogar com ou sem fones de ouvido",
@@ -60,7 +60,7 @@ const products = [
         name: "Oculos de Realidade Virtual Oculos Quest 3, Branco",
         price: 4487.67,
         mark:"Oculos",
-        portion: 5464.20,
+        portion: 5264.20,
         description: {
             color: "Branco",
             particulars:"Hardware de próximo nível - Faça com que cada movimento conte com um processador extremamente rápido e nossa tela de mais alta resolução Jogos All-In-One - Com compatibilidade com versões anteriores, você pode explorar novos títulos e antigos favoritos na extensa biblioteca de conteúdo da Quest Entretenimento imersivo - obtenha o melhor lugar da casa para shows ao vivo, filmes inovadores, eventos exclusivos e muito mais Configuração fácil - basta abrir a caixa, configurar o aplicativo do smartphone e pular para a realidade virtual. Nenhum PC ou console necessário. Requer acesso sem fio à Internet e o aplicativo Oculus (download gratuito) para configurar o dispositivo Tela Premium - capture todos os detalhes com uma tela impressionante que apresenta 50% mais pixels do que a Quest original Controle final - controladores Oculus Touch redesenhados transportam seus movimentos diretamente para a RV com controles intuitivos Compatível com PC VR - Entre em incríveis títulos Oculus Rift conectando um cabo Oculus Link a um PC de jogos compatível. Oculus Link Cable vendido separadamente Som cinematográfico 3D - Ouça em todas as direções com alto-falantes integrados que oferecem áudio posicional 3D cinematográfico.",
@@ -76,9 +76,9 @@ const products = [
     {
         id: 5,
         name: "Oculos de Realidade Virtual Oculus Rift S, Preto",
-        price: 9562.41,
+        price: 5731.46,
         mark:"Oculos",
-        portion: 11249.89,
+        portion: 6140.22,
         description: {
             color: "Preto",
             particulars:"Melhor biblioteca de jogos de realidade virtual: exploda, arraste e voe pela melhor biblioteca de jogos de realidade virtual. Oculus Rift S permite que você jogue centenas de jogos e exclusividades já disponíveis na loja Oculus, com muito mais por vir. Óptica aprimorada: Enfrente a concorrência com lentes de última geração e uma tela mais nítida - Ótica aprimorada oferece cores brilhantes, vivas e reduzidas Efeito 'porta de tela'. Design ergonômico: mantenha sua cabeça no jogo graças a uma faixa de cabeça de halo redesenhada com a velocidade em mente - Rift S permanece segura e confortavelmente no lugar com um giro rápido da roda de ajuste, para que possa pegar ou dobrar tome suas reações mais rápidas.Controladores de toque Oculus: seus golpes, arremessos e agarramentos aparecem em RV com precisão intuitiva e realista, transportando suas mãos e gestos direto para o jogo. Rastreamento Oculus Insight: Oculus Insight traduz seus movimentos em RV, não importa para onde você está olhando e fornece rastreamento em escala de sala sem sensores externos.",
@@ -95,7 +95,7 @@ const products = [
         name: "Oculos de Realidade Virtual PlayStation®VR2, 256GB, Branco",
         price: 4409.99,
         mark:"Sony",
-        portion: 4900.99,
+        portion: 4910.99,
         description: {
             color: "Branco",
             particulars:"Experimente a próxima geração de jogos de realidade virtual com imagens 4K HDR impressionantes, jogos que definem o gênero e sensações únicas com o inovador headset PlayStation VR2 e controle PlayStation VR2 Sense™.",
